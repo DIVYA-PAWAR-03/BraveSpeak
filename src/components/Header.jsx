76cfcd5201e07a7ae89d0b17342ad50a29d24363
DIVYA-PAWAR-03 <121420835+DIVYA-PAWAR-03@users.noreply.github.com>
2025-06-20 17:19:39 +0530

    Header Page - build a nav section

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <>
+        <header>
+            <nav className="navbar">
+                <div className="logo">
+                    <Link to={"/"}>
+                    <h1>BraveSpeak</h1>
+                    </Link>
+                </div>
+            </nav>
+        </header>
+        </>
+    )
+}
