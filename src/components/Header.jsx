80819c32466d9702005c8448225fe32d27fbd8a6
DIVYA-PAWAR-03 <121420835+DIVYA-PAWAR-03@users.noreply.github.com>
2025-06-21 21:05:32 +0530

    Minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,29 @@ export default function Header(){
     return(
         <>
         <header>
-            <nav className="navbar flex justify-between px-6  py-6 items-center  bg-[#880808] text-white">
+            <nav className="navbar flex justify-between px-6  py-6 items-center  bg-[#2E003E] text-white ">
                 <div className="logo">
                     <Link to={"/"}  >
-                    <h1 className=''>BraveSpeak</h1>
+                    <h1 className='text-3xl font-bold'>BraveSpeak</h1>
                     </Link>
                 </div>
-                    <ul className="flex space-x-5">
-                        <li>Home</li>
-                         <li>Stories</li>
-                          <li>Legal Aid</li>
-                           <li>Help</li>
-                           <li>Blog</li>
-                    </ul>
+                    <ul className="flex space-x-6">
+          <li>
+            <Link to="/" className="hover:text-red-400 text-[20px]">Home</Link>
+          </li>
+          <li>
+            <Link to="/statistics" className="hover:text-red-400  text-[20px]">Statistics</Link>
+          </li>
+          <li>
+            <Link to="/laws" className="hover:text-red-400  text-[20px]">Laws</Link>
+          </li>
+          <li>
+            <Link to="/support" className="hover:text-red-400  text-[20px]">Support</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-red-400  text-[20px]">Contact</Link>
+          </li>
+        </ul>
                  </nav>
         </header> 
         
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -3,15 +3,23 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 export default function Landingpage(){
     return(
-        <main className= "bg-cover h-screen" style={{ backgroundImage: "url('/images/bg-main.jpg')"}}>
+        <main className= "bg-[#D6B4FC]  ">
         <div className='flex justify-around items-center'>
-            <div className="left-section mt-10 px-44">
-           <img src="/images/break-chain.png" alt="" className='h-[400px]' />
+            <div className="left-section mt-10 ">
+           <img src="/images/break-chain.png" alt=""/>
             </div>
-            <div className="right-section px-44 mt-30 flex flex-col space-y-8">
-                <h1 className='text-white text-5xl text-justify '>“Breaking the <br /> Silence, Seeking <br /> Justice”</h1>
-                <button className='text-white px-3 py-4 bg-red-900 rounded-4xl'>Know your rights</button>
+            <div className="right-section  mt-20 flex flex-col space-y-8">
+                <h1 className='text-[#2E003E] text-5xl text-justify font-bold '>“Breaking the <br /> Silence, Seeking <br /> Justice”</h1>
+                <button className='text-white px-3 py-4 bg-[#6A0DAD] rounded-4xl font-medium'>Know your rights</button>
             </div>
+        </div>
+        <div className='mt-30 text-center' >
+            <p className='text-purple-900 font-medium'>Every voice matters. Every story deserves to be heard. <br />
+  For far too long, survivors of sexual violence have been silenced by fear, shame, or stigma. But silence protects no one. <br />
+
+We stand with the brave individuals who speak up — not just for themselves, but for those who still can’t. <br />
+This platform is a safe space for support, awareness, and action. Whether you're a survivor, ally, or advocate, your presence here fuels change. <br />
+ <b className='bg-[#2E003E] text-white'>Break the silence. Speak your truth. Justice begins with your voice.</b></p>
         </div>
         </main>
     )
