39d29f64c61098c92f83e6364abeb105ad76b977
DIVYA-PAWAR-03 <121420835+DIVYA-PAWAR-03@users.noreply.github.com>
2025-07-06 20:03:25 +0530

    make storiesPage responsive

--- src/pages/StoriesPage.jsx
+++ src/pages/StoriesPage.jsx
@@ -13,28 +13,28 @@ export default function StoriesPage() {
           Survivor Stories
         </h1>
 
-       
-        <div className='flex space-x-4'>
-          <img src="images/news_6.png" alt="" className='h-[500px]' />
-          <img src="images/news_7.png" alt="" className='h-[500px] mt-6' />
-          <img src="images/news_8.png" alt="" className='h-[500px]' />
+        {/* Row 1 */}
+        <div className='flex flex-col md:flex-row justify-center gap-4'>
+          <img src="images/news_6.png" alt="" className='w-full md:w-auto h-auto md:h-[500px] rounded-xl' />
+          <img src="images/news_7.png" alt="" className='w-full md:w-auto h-auto md:h-[500px] mt-0 md:mt-6 rounded-xl' />
+          <img src="images/news_8.png" alt="" className='w-full md:w-auto h-auto md:h-[500px] rounded-xl' />
         </div>
 
-        <div className='flex mt-5'>
-          <img src="images/news_9.png" alt="" className='h-[350px]' />
-          <img src="images/news_11.webp" alt="" className='h-[350px]' />
-           <img src="images/news_3.jpg" alt="" className='h-[350px]' />
+        {/* Row 2 */}
+        <div className='flex flex-col md:flex-row justify-center gap-4 mt-5'>
+          <img src="images/news_9.png" alt="" className='w-full md:w-auto h-auto md:h-[350px] rounded-xl' />
+          <img src="images/news_11.webp" alt="" className='w-full md:w-auto h-auto md:h-[350px] rounded-xl' />
+          <img src="images/news_3.jpg" alt="" className='w-full md:w-auto h-auto md:h-[350px] rounded-xl' />
         </div>
 
-       
-        <div className='flex justify-end'>
-          <img src="images/news_10.webp" alt="" className='h-[300px]' />
-         
-          <img src="images/news_4.jpeg" alt="" className='h-[250px]' />
+        {/* Row 3 */}
+        <div className='flex flex-col md:flex-row justify-end gap-4 mt-5'>
+          <img src="images/news_10.webp" alt="" className='w-full md:w-auto h-auto md:h-[300px] rounded-xl' />
+          <img src="images/news_4.jpeg" alt="" className='w-full md:w-auto h-auto md:h-[250px] rounded-xl' />
         </div>
       </section>
 
-    
+      {/* Button */}
       <div className="flex justify-center mt-8 mb-10">
         <button
           onClick={ReportHandler}
